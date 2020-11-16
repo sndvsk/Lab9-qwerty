2119614ba84bd506864b17b8dab8d97c157d3082
JelizavetaKuznetsova <jelizaveta.kuznetsova@gmail.com>
2020-11-16 13:55:43 +0200

    SE-70 History tab layout fineshed, part of controller

--- SaleSystemGUI/src/main/java/ee/ut/math/tvt/salessystem/ui/controllers/HistoryController.java
+++ SaleSystemGUI/src/main/java/ee/ut/math/tvt/salessystem/ui/controllers/HistoryController.java
@@ -1,6 +1,13 @@
 package ee.ut.math.tvt.salessystem.ui.controllers;
 
+import ee.ut.math.tvt.salessystem.dao.HibernateSalesSystemDAO;
+import ee.ut.math.tvt.salessystem.dao.SalesSystemDAO;
+import ee.ut.math.tvt.salessystem.dataobjects.Purchase;
+import ee.ut.math.tvt.salessystem.dataobjects.SoldItem;
+import ee.ut.math.tvt.salessystem.logic.ShoppingCart;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -14,6 +21,30 @@ import java.util.ResourceBundle;
 public class HistoryController implements Initializable {
 
     private static final Logger log = LogManager.getLogger(PurchaseController.class);
+    private final SalesSystemDAO dao;
+    private final ShoppingCart shoppingCart;
+
+    @FXML
+    private Button showBetween;
+    @FXML
+    private Button showLast10;
+    @FXML
+    private Button showAll;
+    @FXML
+    private DatePicker startDate;
+    @FXML
+    private DatePicker endDate;
+    @FXML
+    private Label totalSum;
+    @FXML
+    private TableView<Purchase> historyTableView;
+    @FXML
+    private TableView<SoldItem> purchaseTableView;
+
+    public HistoryController() {
+        dao = new HibernateSalesSystemDAO();
+        shoppingCart = new ShoppingCart(dao);
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
