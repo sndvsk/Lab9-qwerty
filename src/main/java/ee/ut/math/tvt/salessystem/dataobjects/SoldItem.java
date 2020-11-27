3d8c774fd4237c29fa9c18c1bdef07155739e757
sndvsk <sander.veske@gmail.com>
2020-11-27 18:53:37 +0200

    persistence file empty + solditem error fix for homework5

--- src/main/java/ee/ut/math/tvt/salessystem/dataobjects/SoldItem.java
+++ src/main/java/ee/ut/math/tvt/salessystem/dataobjects/SoldItem.java
@@ -1,5 +1,5 @@
 package ee.ut.math.tvt.salessystem.dataobjects;
-
+import javax.persistence.*;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
@@ -7,54 +7,39 @@ import javax.persistence.Table;
 /**
  * Already bought StockItem. SoldItem duplicates name and price for preserving history.
  */
-//@Entity
-//@Table(name = "SOLDITEM")
-// extends Item
-public class SoldItem {
+@Entity
+@Table(name = "SOLDITEM")
+public class SoldItem extends Item{
 
+    @Id
     private Long id;
-    private StockItem stockItem;
+
+    @Column(name = "name")
     private String name;
-    private Integer quantity;
+
+    @Column(name = "price")
     private double price;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "quantity")
+    private int quantity;
+
+    @OneToOne
+    //@JoinColumn(name = "id")
+    @JoinColumn(name = "stock_item_id")
+    private StockItem stockItem;
 
     public SoldItem() {
     }
 
     public SoldItem(StockItem stockItem, int quantity) {
         this.id = stockItem.getId();
-        //this.stockItem = stockItem;
+        this.stockItem = stockItem;
         this.name = stockItem.getName();
         this.price = stockItem.getPrice();
         this.quantity = quantity;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public Integer getQuantity() {
-        return quantity;
     }
 
     public void setQuantity(Integer quantity) {
@@ -66,12 +51,11 @@ public class SoldItem {
     }
 
     public StockItem getStockItem() {
-        return null;
-        //return stockItem;
+        return stockItem;
     }
 
     public void setStockItem(StockItem stockItem) {
-        //this.stockItem = stockItem;
+        this.stockItem = stockItem;
     }
 
     @Override
